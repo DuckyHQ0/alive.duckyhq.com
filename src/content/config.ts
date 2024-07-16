1d1f9c039f561a01539655b0b326bd8638cd8cd1
dukc <dukkcc@proton.me>
2024-07-15 17:21:03 -0700

    mobile responsive

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const blog = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    description: z.string(),
     image: z.string(),
     author: z.string(),
     date: z.string(),
